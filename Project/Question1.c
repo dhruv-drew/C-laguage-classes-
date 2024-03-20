03b2e10c707b97838c2abf1ab1fd34a7e8c2d067
dhruv-drew <152526475+dhruv-drew@users.noreply.github.com>
2024-03-20 14:48:52 +0530

    Project

---
+++ Project/Question1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    char alpha = 'a';
+
+    do
+    {
+        /* code */
+        printf("%c\n",alpha);
+        alpha+=4;
+    } while ( alpha <= 'z');
+     
+
+     return 0;
+}
