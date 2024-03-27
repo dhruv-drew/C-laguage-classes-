2956afa558dfbab82aa4e6b15f76945e62ca241f
dhruv-drew <152526475+dhruv-drew@users.noreply.github.com>
2024-03-27 15:19:50 +0530

    Revision

---
+++ Revision/Question3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    for (int i = 5; i >= 1; i--)
+    {
+        for (int s = i; s > 1; s--)
+        {
+            printf(" ");
+        }
+        for (int j = i; j <= 5; j++)
+        {
+            printf("%d", j);
+        }
+        for (int j = 5; j > i; j--)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
